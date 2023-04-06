08e792f069e80b1838bea53ec9db18be6afb8184
Gowtham-Chokkalingam <gowthamtceian@gmail.com>
2023-04-06 10:54:14 +0530

    aggregation framework

--- MongoDB/Aggregation/Operations/Practice2.js
+++ MongoDB/Aggregation/Operations/Practice2.js
@@ -1,6 +1,16 @@
 // db b21 collecrions heros and orders
 
 db.orders.find();
-db.heros.find()
+db.heros.find();
 
-db.orders.aggregate() // This will gives us all the record present in the collection
+db.orders.aggregate(); // This will gives us all the record present in the collection
+
+db.orders.aggregate({ $limit: 2 }); // which gives output as two records only
+
+//? Sorting price in ascending order
+
+db.orders.aggregate([{ $sort: { price: 1 } }]);
+
+//? To find the all small size rec price greater than 16
+
+db.orders.aggregate([{ $match: { size: "small", price: { $gt: 16 } } }]);
